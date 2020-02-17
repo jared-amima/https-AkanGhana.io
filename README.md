d365eedc76557f80ab83950bee4c66c27107b9eb
JARED <amimajared3@gmail.com>
2020-02-17 11:18:23 +0300

    modify css/styles.css

---
+++ README.md
@@ -0,0 +1,16 @@
+# Project Name
+ Akan-Ip
+
+## Author
+ * Jared Amima 
+
+  ## Description of the project
+  *a project build on a web application that takes care of user's birthday and calculates the day of the week they were born depending on their gender ouput their Akan names
+
+## set up insructions   
+* good network
+* click the link <a href="">here</a>
+
+### license
+*{mit}*
+copyright (c) (2020) **{jared-amima}**
---
+++ js/scripts.js
@@ -0,0 +1,102 @@
+function getName(){
+
+
+var MM = document.querySelector("#month").value;
+var YYYY = document.querySelector("#year").value;
+var DD = document.querySelector("#day").value;
+var gender = document.querySelector('input[name="gender"]:checked').value;
+
+
+if ((MM=="") || (isNaN(MM) || (MM < 0) || (MM>12))) {
+alert('Invalid Month! Please enter correct Month');
+return;
+}
+
+if ((YYYY=="") || (isNaN(YYYY) || (YYYY < 0))) {
+alert('Invalid Year! Please enter correct Year');
+return;
+}
+
+
+if ((DD=="") || (isNaN(DD) || (DD < 0) || (DD>31))) {
+alert('Invalid Day of the Month! Please enter correct Day of the Month');
+return;
+}
+
+
+var CC = YYYY.substring(0, 2)
+var YY = YYYY.substring(2, 4)
+
+
+var birthday = new Date(YYYY, MM, DD);
+var dayOfTheWeek = birthday.getDay();
+
+var name = "";
+// Sunday
+if (dayOfTheWeek==1){
+if(gender=="male"){
+name = "Kwasi"
+} else {
+name = "Akosua"
+}
+}
+
+// Monday
+if (dayOfTheWeek==2){
+if(gender=="male"){
+name ="Kwadwo"
+} else {
+name ="Adwoa"
+}
+}
+
+// Tuesday
+if (dayOfTheWeek==3){
+if(gender=="male"){
+name ="Kwabena"
+} else {
+name ="Abenaa"
+}
+}
+// Wednesday
+if (dayOfTheWeek==4){
+if(gender=="male"){
+name ="Kwaku"
+} else {
+name ="Akua"
+}
+}
+// Thursday
+if (dayOfTheWeek==5){
+if(gender=="male"){
+name ="Yaw"
+} else {
+name ="yaa"
+}
+}
+// Friday
+if (dayOfTheWeek==6){
+if(gender=="male"){
+name ="Kofi"
+} else {
+name ="Afua"
+}
+}
+// Saturday
+if (dayOfTheWeek==7){
+if(gender=="male"){
+name ="Kwame"
+} else {
+name ="Ama"
+}
+}
+
+alert(
+'The month is ' + MM + '\n'+
+'The century is ' + CC+ '\n'+
+'The year is ' + YY+ '\n'+
+'The day is ' + DD + '\n'+
+'The gender is ' + gender + '\n'+
+'The day of the week born is ' + dayOfTheWeek + '\n'+
+'Your Akan name is ' + name )
+}
